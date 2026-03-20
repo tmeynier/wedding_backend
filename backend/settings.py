4958436c206c0f55a6ceca6aa6dab27250586dad
Tristan <meyniertristan10@gmail.com>
2026-03-20 18:49:59 +0100

    test(settings): test update settings

--- backend/settings.py
+++ backend/settings.py
@@ -92,23 +92,23 @@ CSRF_TRUSTED_ORIGINS = [
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME", "testdb"),         # Falls back to local testdb
-        "USER": os.environ.get("DB_USER", "postgres"),
-        "PASSWORD": os.environ.get("DB_PASSWORD", "postgres"),
-        "HOST": os.environ.get("DB_HOST", "localhost"),
-        "PORT": os.environ.get("DB_PORT", "5432"),
-    }
-}
-
 #DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
+#    "default": {
+#        "ENGINE": "django.db.backends.postgresql",
+#        "NAME": os.environ.get("DB_NAME", "testdb"),         # Falls back to local testdb
+#        "USER": os.environ.get("DB_USER", "postgres"),
+#        "PASSWORD": os.environ.get("DB_PASSWORD", "postgres"),
+#        "HOST": os.environ.get("DB_HOST", "localhost"),
+#        "PORT": os.environ.get("DB_PORT", "5432"),
 #    }
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
